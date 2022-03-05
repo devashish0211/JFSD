cb66422bb5c32865f390adc70d16ab0e310520c8
devashish0211 <devashishsinha662@gmail.com>
2022-03-05 15:41:46 +0530

    3.04 Sleep&Wait added

---
+++ 3.04 Sleep&Wait/src/sleep_wait/Sleep_and_Wait.java
@@ -0,0 +1,18 @@
+package sleep_wait;
+
+public class Sleep_and_Wait 
+{
+	private static Object obj = new Object();   
+	
+	public static void main(String[] args) throws InterruptedException 
+	{
+		Thread.sleep(2000);
+		System.out.println( Thread.currentThread().getName() +" Thread is woken after two second"); 
+		  synchronized (obj)    
+	        {   
+	            obj.wait(2000);   
+	            System.out.println(obj + " Object is in waiting state and woken after 2 seconds");   
+	        }  
+	}
+
+}
