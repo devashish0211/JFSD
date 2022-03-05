d1e14ae7bbabad7ec5da8b9d50cdbb08768c7b52
devashish0211 <devashishsinha662@gmail.com>
2022-03-05 15:45:17 +0530

    3.10 Exception_Handler added

---
+++ 3.10 Exception_Handler/src/exception_handler/Exception_Handler.java
@@ -0,0 +1,20 @@
+package exception_handler;
+
+public class Exception_Handler 
+{
+
+	public static void main(String[] args) 
+		{
+			try
+			{  
+				
+			}
+			catch(ArithmeticException e)
+				{
+					System.out.println(e);
+				}  
+		 
+			System.out.println("rest of the code here....");  
+		 }
+
+}
