15685a5fb19f8a9bf9b613a7836b1aefb759c522
devashish0211 <devashishsinha662@gmail.com>
2022-03-07 10:16:47 +0530

    Phase -1-Assessment_Project added

---
+++ Phase -1-Assessment_Project/src/lokedme/Main.java
@@ -0,0 +1,15 @@
+package lokedme;
+
+public class Main {
+
+	/*Enter your desired Directory path */
+	public static final String path = "C:\\Users\\devas\\OneDrive\\Desktop\\Demo";
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Menu menu = new Menu();
+		menu.introScreen();
+		menu.mainMenu();
+	}
+
+}
